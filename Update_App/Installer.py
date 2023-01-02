6d9bf51d0a6ac8ef127d9a720fcd8b2a969fe64a
Superior-GitHub <vip.klochayil@gmail.com>
2023-01-03 02:41:52 +0300

    Update Updater :)

--- Update_App/Installer.py
+++ Update_App/Installer.py
@@ -48,33 +48,67 @@ def delete_zip(file):
 
 
 def clear_catalog(path):
-    try:
-        file_list = os.listdir(path)
-        # print(file_list)
-        for item in file_list:
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_list = os.listdir(path)
+    # print(file_list)
+    for item in file_list:
+        # print(item)
+        # if item != "Python3109" and item != "Update.bat" and item != "Update_files":
+        s = os.path.join(path, item)
+        if os.path.isdir(s):
+            try:
+                shutil.rmtree(s)
+            except OSError as e:
+                pass
+                # print("Error: %s - %s." % (e.filename, e.strerror))
+        else:
+            os.remove(s)
+
+
+def get_zip(file, url):
+    print("Getting zip of superior6564App.")
+    with open(file, "wb") as new_file:
+        new_file.write(requests.get(url).content)
+
+
+def extract_zip(file, path):
+    print(f"Extract the zip file to {path}.")
+    with zipfile.ZipFile(file, 'r') as zip_file:
+        zip_file.extractall(path)
+    time.sleep(5)
+
+
+def copytree(path_from, path_to, black_list: list = None, symlinks=False, ignore=None):
+    if not os.path.exists(path_to):
+        os.makedirs(path_to)
+    for item in os.listdir(path_from):
+        if item not in black_list:
             # print(item)
-            # if item != "Python3109" and item != "Update.bat" and item != "Update_files":
-            s = os.path.join(path, item)
+            # s = os.path.join(path_from, item)
+            s = f"{path_from}/{item}"
+            # d = os.path.join(path_to, item)
+            d = f"{path_to}/{item}"
             if os.path.isdir(s):
-                try:
-                    shutil.rmtree(s)
-                except OSError as e:
-                    pass
-                    # print("Error: %s - %s." % (e.filename, e.strerror))
+                copytree(s, d, black_list, symlinks, ignore)
             else:
-                os.remove(s)
-    except FileNotFoundError:
-        pass
+                if item == "Python3109.zip":
+                    extract_zip(f"{path_from}/{item}", path_to)
+                else:
+                    if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                        shutil.copy2(s, d)
 
 
 def transfer_folders_files(path_from: str, path_to: str, black_list: list = None):
     print(f"Transfer folders and files from temp.")
-    full_list = os.listdir(path_from)
+    copytree(path_from, path_to, black_list)
+    # full_list = os.listdir(path_from)
     # print(full_list)
-    for item in full_list:
-        if item not in black_list:
+    # print(full_list)
+    # for item in full_list:
+        # if item not in black_list:
             # print(item)
-            if item == "Python3109":
+            # if item == "Python3109.zip":
                 # def copytree(src, dst, symlinks=False, ignore=None):
                 #     if not os.path.exists(dst):
                 #         os.makedirs(dst)
@@ -89,23 +123,11 @@ def transfer_folders_files(path_from: str, path_to: str, black_list: list = None
 
                 # copytree(f"{path_from}/{item}", f"{black_path_to}/{item}")
                 # copytree(f"{path_from}/{item}", f"{path_to}/{item}")
-                shutil.copytree(f"{path_from}/{item}", f"{path_to}/{item}", symlinks=False, ignore=None)
+                # shutil.copytree(f"{path_from}/{item}", f"{path_to}/{item}", symlinks=False, ignore=None)
                 # os.rename(f"{path_from}/{item}", f"{path_to}/{item}")
-            else:
-                os.rename(f"{path_from}/{item}", f"{path_to}/{item}")
-
-
-def get_zip(file, url):
-    print("Getting zip of superior6564App.")
-    with open(file, "wb") as new_file:
-        new_file.write(requests.get(url).content)
-
-
-def extract_zip(file, path):
-    print(f"Extract the zip file to {path}.")
-    with zipfile.ZipFile(file, 'r') as zip_file:
-        zip_file.extractall(path)
-    time.sleep(5)
+                # extract_zip(f"{path_from}/{item}", f"{common_path}/superior6564App")
+            # else:
+            #     os.rename(f"{path_from}/{item}", f"{path_to}/{item}")
 
 
 def create_shortcut(file, target, icon, work_dir, target_dir=None):
@@ -143,24 +165,27 @@ while agree != "":
     agree = input("Press 'enter' to continue or write 'exit' to exit: ")
     if agree == "exit":
         exit()
-common_path = "C:/superior6564"
+common_path = "C:/"
+
+if not os.path.exists("C:/superior6564"):
+    os.makedirs("C:/superior6564")
 
 with open("C:/superior6564/path_app.txt", "w") as path_file:
     path_file.write(common_path)
 
 print("Deleting previous application.")
-clear_catalog(f"{common_path}/temp")
-clear_catalog(f"{common_path}/superior6564App")
+clear_catalog(f"{common_path}/superior6564/temp")
+clear_catalog(f"{common_path}/superior6564/superior6564App")
 delete_file(f"{winshell.desktop()}/superior6564App.lnk", )
 delete_file(f"{winshell.desktop()}/Update.lnk", )
 delete_zip("superior6564App.zip")
 get_zip("superior6564App.zip", "https://github.com/Superior-GitHub/superior6564App/archive/refs/heads/master.zip")
-extract_zip("superior6564App.zip", f"{common_path}/temp")
-transfer_folders_files(f"{common_path}/temp/superior6564App-master",
-                       f"{common_path}/superior6564App",
+extract_zip("superior6564App.zip", f"{common_path}/superior6564/temp")
+transfer_folders_files(f"{common_path}/superior6564/temp/superior6564App-master",
+                       f"{common_path}/superior6564/superior6564App",
                        ["Update.bat", "Update_App", "Update_Updater.bat", "Update_Updater"])
-create_shortcut("superior6564App.lnk", "GeneratorRUWords.bat", "Photos_or_Icons/degget_6564.ico", f"{common_path}/superior6564App")
-path = f"{common_path}/temp/superior6564App-master/Update_Updater.bat"
+create_shortcut("superior6564App.lnk", "GeneratorRUWords.bat", "Photos_or_Icons/degget_6564.ico", f"{common_path}/superior6564/superior6564App")
+path = f"{common_path}/superior6564/temp/superior6564App-master/Update_Updater.bat"
 subprocess.Popen(path)
 # create_shortcut("Update.lnk", f"Update.bat", "Photos_or_Icons/degget_6564.ico", os.getcwd())
 # print("Finish.")
--- Update_Updater/Update_Updater.py
+++ Update_Updater/Update_Updater.py
@@ -1,6 +1,7 @@
 import os
 import time
 import shutil
+import zipfile
 import winshell
 from win32com.client import Dispatch
 
@@ -14,6 +15,8 @@ def kill_process(seconds):
 
 
 def clear_catalog(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
     file_list = os.listdir(path)
     # print(file_list)
     for item in file_list:
@@ -30,28 +33,39 @@ def clear_catalog(path):
             os.remove(s)
 
 
-def copytree(path_from, path_to, symlinks=False, ignore=None):
+def extract_zip(file, path):
+    print(f"Extract the zip file to {path}.")
+    with zipfile.ZipFile(file, 'r') as zip_file:
+        zip_file.extractall(path)
+    time.sleep(5)
+
+
+def copytree(path_from, path_to, black_list: list = None, symlinks=False, ignore=None):
     if not os.path.exists(path_to):
         os.makedirs(path_to)
     for item in os.listdir(path_from):
-        # print(item)
-        # s = os.path.join(path_from, item)
-        s = f"{path_from}/{item}"
-        # d = os.path.join(path_to, item)
-        d = f"{path_to}/{item}"
-        if os.path.isdir(s):
-            copytree(s, d, symlinks, ignore)
-        else:
-            if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
-                shutil.copy2(s, d)
+        if item in black_list:
+            # print(item)
+            # s = os.path.join(path_from, item)
+            s = f"{path_from}/{item}"
+            # d = os.path.join(path_to, item)
+            d = f"{path_to}/{item}"
+            if os.path.isdir(s):
+                copytree(s, d, black_list, symlinks, ignore)
+            else:
+                if item == "Python3109.zip":
+                    extract_zip(f"{path_from}/{item}", path_to)
+                else:
+                    if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                        shutil.copy2(s, d)
 
 
-def transfer_folders_files(path_from: str, path_to: str):
+def transfer_folders_files(path_from: str, path_to: str, black_list: list = None):
     print(f"Transfer folders and files from temp.")
     # full_list = os.listdir(path_from)
     # print(full_list)
     # for item in full_list:
-    copytree(path_from, path_to)
+    copytree(path_from, path_to, black_list)
     time.sleep(5)
     # if os.path.isdir(item):
     # if item == "Python3109" or item == "Update_Updater" or item == "Update_App":
@@ -85,8 +99,11 @@ with open("C:/superior6564/path_app.txt", "r") as path_file:
 # exit()
 
 # common_path = r"C:\superior6564"
-clear_catalog(f"{common_path}/superior6564AppUpdater")
-transfer_folders_files(f"{common_path}/temp/superior6564App-master", f"{common_path}/superior6564AppUpdater")
-create_shortcut("Update.lnk", f"Update.bat", "Photos_or_Icons/degget_6564.ico", f"{common_path}/superior6564App", f"{common_path}/superior6564AppUpdater")
+clear_catalog(f"{common_path}/superior6564/superior6564AppUpdater")
+transfer_folders_files(f"{common_path}/superior6564/temp/superior6564App-master",
+                       f"{common_path}/superior6564/superior6564AppUpdater",
+                       ["Update.bat", "Update_App", "Update_Updater.bat", "Update_Updater",
+                        "Python3109.zip", "Check_Packages.py", "Installer.py", "Update_Updater.py"])
+create_shortcut("Update.lnk", f"Update.bat", "Photos_or_Icons/degget_6564.ico", f"{common_path}/superior6564/superior6564App", f"{common_path}/superior6564/superior6564AppUpdater")
 print("Finish.")
 kill_process(10)

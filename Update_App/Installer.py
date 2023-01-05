ef092db79905a0703895f4706de4dde995a41696
Superior-GitHub <vip.klochayil@gmail.com>
2023-01-05 12:26:16 +0300

    Edited choosing path to install app :)

--- Update_App/Installer.py
+++ Update_App/Installer.py
@@ -67,6 +67,40 @@ def clear_catalog(path):
             os.remove(s)
 
 
+def get_path_for_app(default_path, app_path):
+    agree = " "
+    common_path = ""
+    while agree != "+" or agree != "-":
+        app_path = "C:/superior6564/path_app.txt"
+        agree = input(f"Default path is {default_path}"
+                      f"\nDo you want to change path for app?"
+                      f"\nWrite '+' to change or '-' to don`t change: ")
+        if agree == "+":
+            common_path = tkinter.filedialog.askdirectory(initialdir=default_path, title="Choose directory")
+            if common_path != default_path:
+                common_path = f"{common_path}/superior6564"
+            check_previous_path = ""
+            if os.path.exists(app_path):
+                with open(app_path, "r") as file_path:
+                    check_previous_path = file_path.readline()
+                if common_path != check_previous_path:
+                    clear_catalog(f"{check_previous_path}/superior6564")
+            if not os.path.exists("C:/superior6564"):
+                os.makedirs("C:/superior6564")
+            with open(app_path, "w") as file_path:
+                file_path.write(common_path)
+            break
+        elif agree == "-":
+            if os.path.exists(app_path):
+                with open(app_path, "r") as file_path:
+                    common_path = file_path.readline()
+            break
+        else:
+            common_path = default_path
+            break
+    return common_path
+
+
 def get_zip(file, url):
     print("Getting zip of superior6564App.")
     with open(file, "wb") as new_file:
@@ -169,33 +203,43 @@ while agree != "":
         exit()
 
 default_path = "C:/superior6564"
-common_path = ""
-while agree != "+" or agree != "-":
-    path_app = "C:/superior6564/path_app.txt"
-    agree = input(f"Default path is {default_path}\nDo you want to change path for app?\nWrite '+' to change or '-' to don`t change: ")
-    if agree == "+":
-        common_path = tkinter.filedialog.askdirectory(initialdir=default_path, title="Choose directory")
-        if common_path != default_path:
-            common_path = f"{common_path}/superior6564"
-        check_previous_path = ""
-        if os.path.exists(path_app):
-            with open(path_app, "r") as path_file:
-                check_previous_path = path_file.readline()
-            if common_path != check_previous_path:
-                clear_catalog(f"{check_previous_path}/superior6564")
-        if not os.path.exists("C:/superior6564"):
-            os.makedirs("C:/superior6564")
-        with open(path_app, "w") as path_file:
-            path_file.write(common_path)
+app_path = "C:/superior6564/path_app.txt"
+common_path = get_path_for_app(default_path=default_path, app_path=app_path)
+
+while agree != "" or agree != "-":
+    agree = input(f"App will install in {common_path}."
+                  f"\nDo you want to change path for app?"
+                  f"\nPress 'enter' to continue or '-' to don`t change: ")
+    if agree == "-":
+        common_path = get_path_for_app(default_path=default_path, app_path=app_path)
         break
-    elif agree == "-":
-        if os.path.exists(path_app):
-            with open(path_app, "r") as path_file:
-                common_path = path_file.readline()
-        break
-    else:
-        common_path = default_path
-        break
+
+# while agree != "+" or agree != "-":
+#     path_app = "C:/superior6564/path_app.txt"
+#     agree = input(f"Default path is {default_path}\nDo you want to change path for app?\nWrite '+' to change or '-' to don`t change: ")
+#     if agree == "+":
+#         common_path = tkinter.filedialog.askdirectory(initialdir=default_path, title="Choose directory")
+#         if common_path != default_path:
+#             common_path = f"{common_path}/superior6564"
+#         check_previous_path = ""
+#         if os.path.exists(path_app):
+#             with open(path_app, "r") as path_file:
+#                 check_previous_path = path_file.readline()
+#             if common_path != check_previous_path:
+#                 clear_catalog(f"{check_previous_path}/superior6564")
+#         if not os.path.exists("C:/superior6564"):
+#             os.makedirs("C:/superior6564")
+#         with open(path_app, "w") as path_file:
+#             path_file.write(common_path)
+#         break
+#     elif agree == "-":
+#         if os.path.exists(path_app):
+#             with open(path_app, "r") as path_file:
+#                 common_path = path_file.readline()
+#         break
+#     else:
+#         common_path = default_path
+#         break
 
 print("Deleting previous application.")
 clear_catalog(f"{common_path}/temp")
